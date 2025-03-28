2d413197f497b67c554c7184adf2dc535a9d8cf5
kishore14194 <thereal.hunter33@gmail.com>
2025-03-28 15:14:58 +0530

    Adding docker compose

--- stock-dashboard/vite.config.js
+++ stock-dashboard/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Change this to your desired port
+    host: "0.0.0.0" // Ensure it listens on all interfaces
+  }
 })
